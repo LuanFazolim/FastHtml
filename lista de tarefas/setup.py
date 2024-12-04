f49bab6e5cb3d66a2a360aefc511a18da82e8949
LuanFazolim <131609670+LuanFazolim@users.noreply.github.com>
2024-12-04 16:03:13 -0300

    Add files via upload

---
+++ lista de tarefas/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FastHtml',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=[
+        # Liste aqui as dependências do seu projeto
+    ],
+)
